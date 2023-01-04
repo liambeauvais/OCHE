4b5e32a84138ed5cdff77207a2ab892ef2027759
Liam <liam.beauvais@gmail.com>
2023-01-04 14:27:11 +0100

    init

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Bonjour")
+import datetime
+
+date = datetime.datetime.now()
+print("Bonsoir" if date.hour > 18 or date.hour < 6 else "Bonjour")
+
+user_input = input("quel est votre mot?")
+response = user_input[::-1]
+print(response)
+if response == user_input:
+    print("Bien dit!")
