2163aa07e2f585d2e2824b3c017e926e53ad3e55
Liam <liam.beauvais@gmail.com>
2023-01-04 14:32:29 +0100

    os language implementation

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import datetime
+import locale
 
 date = datetime.datetime.now()
-print("Bonsoir" if date.hour > 18 or date.hour < 6 else "Bonjour")
+if locale.getdefaultlocale()[0] == "fr_FR":
+    print("Bonsoir" if date.hour > 18 or date.hour < 6 else "Bonjour")
+else:
+    print("Good evening" if date.hour > 18 or date.hour < 6 else "Good morning")
 
 user_input = input("quel est votre mot?")
 response = user_input[::-1]
